8941f4f9a010bce1a6ffb433e59bfaa221f6a711
ali mujib <amujib42503@gmail.com>
2025-07-23 08:29:16 -0400

    Complete exercises 1-7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,27 @@
-const sumAll = function() {
+const sumAll = function(firstNum, lastNum) {
+
+if (typeof firstNum !== "number" || typeof lastNum !== "number") {
+    return "ERROR";
+}
+
+if (!Number.isInteger(firstNum) || !Number.isInteger(lastNum)) {
+    return "ERROR";
+}
+
+if (firstNum < 0 || lastNum < 0) {
+    return "ERROR";
+}
+
+if (firstNum > lastNum) {
+    [firstNum, lastNum] = [lastNum, firstNum];
+}
+
+let finalSum = 0;
+for (let i = firstNum; i <= lastNum; i++) {
+    finalSum += i;
+}
+
+return finalSum;
 
 };
 
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(C) {
+
+F = (C -32) * 5/9;
+return +F.toFixed(1);
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(F) {
+
+C = (F * 9/5 + 32);
+return +C.toFixed(1); 
+
 };
 
 // Do not edit below this line
